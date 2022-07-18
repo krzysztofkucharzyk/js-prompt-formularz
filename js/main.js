980cbcec4e97d0e582135c3d80efc45129aa6ba7
Krzysztof Kucharzyk <krzysztofkucharzyk@gmail.com>
2022-07-18 23:30:19 +0200

    pobranie danych

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let i = prompt('Podaj imię');
+let s = prompt('Podaj nazwisko');
+let e = prompt('Podaj email');
+
+function pobierzDane () {
+    let inputName = document.getElementById('fname').value = i;
+    let inputSname = document.getElementById('sname').value = s;
+    let inputEmail = document.getElementById('email').value = e;
+
+}
+
+pobierzDane();
